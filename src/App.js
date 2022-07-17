00a6b672918404d3b6879f6729f6354cbb9d8b01
Karan Sehgal <sehgalk043@gmail.com>
2022-07-17 19:43:48 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar.js"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Slider from "./components/Slider.js"
 // import data from "./data/data"
+
 import data from "./data/data.json"
 import Offers from "./components/Offers.js"
 import Heading from "./components/Heading.js"
